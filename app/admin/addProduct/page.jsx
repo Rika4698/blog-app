7791387bcb6280afae4ebba0ccb4aa3f19dddda6
Rika4698 <rikasharmin181@gmail.com>
2025-04-15 22:29:07 +0600

    connect admin panel

---
+++ app/admin/addProduct/page.jsx
@@ -0,0 +1,21 @@
+'use client'
+import { assets } from '@/Assets/assets';
+import Image from 'next/image';
+import React from 'react';
+
+const page = () => {
+    return (
+        <>
+         <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+            <p className='text-xl'>Upload thumbnail</p>
+            <label htmlFor="">
+                <Image className='mt-4' src={assets.upload_area} width={140} hight={70} alt=''/>
+            </label>
+            <input type="file" id='image' hidden required />
+
+            </form>   
+        </>
+    );
+};
+
+export default page;
